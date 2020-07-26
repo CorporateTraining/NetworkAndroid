f235a71e5ea91228de69b4adc324dc54bdc3bc4d
mahuanfy <mahuanfy@foxmail.com>
2020-07-27 00:41:50 +0800

    [Huanhuan]: finish network  practice.

--- app/src/main/java/com/example/networkandroid/MainActivity.java
+++ app/src/main/java/com/example/networkandroid/MainActivity.java
@@ -3,12 +3,64 @@ package com.example.networkandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button networkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        networkButton = findViewById(R.id.network_activity);
+        networkButton.setOnClickListener((view -> {
+            String url = "https://twc-android-bootcamp.github.io/fake-data/data/default.json";
+            Observable<String> objectObservable = Observable.create(item -> {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request
+                        .Builder()
+                        .url(url)
+                        .build();
+                Response response = okHttpClient.newCall(request)
+                        .execute();
+                String data = response.body().string();
+                item.onNext(data);
+                item.onComplete();
+            });
+            objectObservable
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Observer<String>() {
+                        @Override
+                        public void onSubscribe(Disposable d) {
+                        }
+
+                        @Override
+                        public void onNext(String s) {
+                            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onComplete() {
+                        }
+                    });
+        }));
     }
 }
